ca57302c9b9ec3ea37c65b4832bc611b843d0d1a
atul kumar <atul.11192@gmail.com>
2020-05-27 03:13:44 +0530

    adding sub category page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 const  expres=require('express');
 const router=expres.Router();
+const Category=require('../models/category');
 router.get('/',(req,res)=>{
     res.render('addcategory');
+});
+router.get('/subcategory',(req,res)=>{
+     Category.findAll().then(result=>{
+        const categories=result;
+       res.render('addsubcategory',{categories});
+     }).catch(err=>{
+        res.status(401).json({
+            error:err
+        });
+     });
 });
 module.exports=router;
--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const PORT=process.env.PORT||8080;
 const subcategory=require('./routes/Subcategoryfunctions');
 const category=require('./routes/Categoryfunctions');
 const route=require('./routes/index');
+const _hnaldebars=require('handlebars');
 const path=require('path');
+const {allowInsecurePrototypeAccess}=require('@handlebars/allow-prototype-access');
 const app=express();
 const exphbs=require('express-handlebars');
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.set('view engine','handlebars');
-app.engine('handlebars',exphbs({defaultlayout:'main'}));
+app.engine('handlebars',exphbs({defaultlayout:'main',
+handlebars:allowInsecurePrototypeAccess(_hnaldebars)
+}));
 app.use(express.static(path.join(__dirname,'public')));
 app.use('/category',category);
 app.use('/',route);
-app.use('/suubcategory',subcategory);
+app.use('/subcategory',subcategory);
 app.listen(PORT,()=>{
     console.log('server started on ' +PORT);
 });
