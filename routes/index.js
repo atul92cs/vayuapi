a5bdc923b98fed421a51bce06760748f5999d8f1
atul kumar <atul.11192@gmail.com>
2020-05-27 22:39:24 +0530

    adding ui for category and subcategory

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,29 @@
 const  expres=require('express');
 const router=expres.Router();
 const Category=require('../models/category');
+const Subcategory=require('../models/subcategory')
 router.get('/',(req,res)=>{
-    res.render('addcategory');
+   Category.findAll({}).then(result=>{
+       const categories=result;
+    res.render('addcategory',{categories});
+   }).catch(err=>{
+       res.status(401).json({
+           message:'error occrued',
+           error:err
+       });
+   });
 });
 router.get('/subcategory',(req,res)=>{
      Category.findAll().then(result=>{
-        const categories=result;
-       res.render('addsubcategory',{categories});
+        Subcategory.findAll().then(sub=>{
+            const categories=result;
+            const subcategories=sub;
+            res.render('addsubcategory',{categories,subcategories});
+        }).catch(err=>{
+            res.status(401).json({
+                error:err
+            });
+        });
      }).catch(err=>{
         res.status(401).json({
             error:err
