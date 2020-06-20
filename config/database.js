076e117fea37a92a9a24e10d315ebf33e2d6fb6f
atul kumar <atul.11192@gmail.com>
2020-06-21 00:40:40 +0530

    adding new database

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mysql=require('mysql2');
 const sequelize=require('sequelize');
-module.exports=new sequelize('IOQyvWVlQq','IOQyvWVlQq','4CLqbgclJt',{
-    host:'remotemysql.com',
+module.exports=new sequelize('bcbajerbdduefqdewhfr','u9li4c89geunsmxy','yPROe5z5tGbOmYYNyjbC',{
+    host:'bcbajerbdduefqdewhfr-mysql.services.clever-cloud.com',
     port:'3306',
     dialect:'mysql'
 });
